8a10e5eba904760a4d23a99d77e9dc4f1a50a22e
Robin Gagnon <me@reobin.dev>
2026-03-24 08:43:47 -0500

    feat: use transparency option on on neovim colorscheme

--- neovim.lua
+++ neovim.lua
@@ -1,16 +1,18 @@
 return {
-  {
-    "reobin/olive-crt.nvim",
-    priority = 1000,
-    opts = {},
-    init = function()
-      vim.o.background = "light"
-    end,
-  },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "olive-crt",
-    },
-  },
+	{
+		"reobin/olive-crt.nvim",
+		priority = 1000,
+		opts = {
+			transparent = true,
+		},
+		init = function()
+			vim.o.background = "light"
+		end,
+	},
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "olive-crt",
+		},
+	},
 }
